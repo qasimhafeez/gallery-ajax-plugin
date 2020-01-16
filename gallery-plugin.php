c0f67131f6a0c5c3379b3de8017ee791644b2556
Qasim Hafeez <qasimh556@gmail.com>
2020-01-16 11:48:18 +0400

    initial commit

--- gallery-plugin.php
+++ gallery-plugin.php
@@ -5,5 +5,9 @@
 /*
 Plugin Name: Gallery Plugin
 Plugin URI: https://www.github.com/qasimhafeez/gallery-plugin
-
+Description: Gallery plugin to load images by categories using AJAX
+Version: 1.0.0
+Author: Qasim Hafeez
+Author URI: https://www.github.com/qasimhafeez
+License: MIT
 */
